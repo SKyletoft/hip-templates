5ae4a804ee38d26976783b1e5c43357f5dd52d79
Samuel Kyletoft <samuel@kyletoft.se>
2025-11-17 12:47:49 +0100

    Add 2d and 3d containers

---
+++ gpu_2d.hpp
@@ -0,0 +1,223 @@
+#pragma once
+
+#include "gpu.hpp"
+
+namespace gpu {
+
+template <typename T>
+	requires std::is_trivially_copyable_v<T>
+class device_span2 : public device_span<T> {
+public:
+	using base = device_span<T>;
+	using typename base::size_type;
+	using typename base::pointer;
+
+private:
+	size_type width_  = 0;
+	size_type height_ = 0;
+	size_type pitch_  = 0;
+
+public:
+	constexpr device_span2() noexcept = default;
+
+	constexpr device_span2(
+		pointer ptr,
+		size_type width,
+		size_type height,
+		size_type pitch
+	) noexcept
+		: base(ptr, width * height)
+		, width_(width)
+		, height_(height)
+		, pitch_(pitch)
+	{}
+
+	[[nodiscard]] constexpr auto width() const noexcept -> size_type { return width_; }
+	[[nodiscard]] constexpr auto height() const noexcept -> size_type { return height_; }
+	[[nodiscard]] constexpr auto pitch() const noexcept -> size_type { return pitch_; }
+
+	[[nodiscard]] __device__ constexpr auto operator()(size_type x, size_type y) -> T& {
+		return this->data_[y * pitch_ + x];
+	}
+
+	[[nodiscard]] __device__ constexpr auto operator()(size_type x, size_type y) const -> const T& {
+		return this->data_[y * pitch_ + x];
+	}
+
+	[[nodiscard]] constexpr auto row(size_type y) const noexcept -> device_span<T> {
+		return device_span<T>(this->data_ + y * pitch_, width_);
+	}
+
+	[[nodiscard]] constexpr auto subspan2(
+		size_type x_offset,
+		size_type y_offset,
+		size_type width,
+		size_type height
+	) const noexcept -> device_span2<T> {
+		return device_span2<T>(
+			this->data_ + y_offset * pitch_ + x_offset,
+			width,
+			height,
+			pitch_
+		);
+	}
+};
+
+template <typename T>
+	requires std::is_trivially_copyable_v<T>
+class device_unique_ptr2 : private device_span2<T> {
+public:
+	using base = device_span2<T>;
+	using typename base::size_type;
+	using typename base::pointer;
+
+	device_unique_ptr2() noexcept = default;
+
+	explicit device_unique_ptr2(size_type width, size_type height)
+		: device_span2<T>(nullptr, width, height, 0)
+	{
+		if (width <= 0 || height <= 0) {
+			return;
+		}
+
+		size_t pitch_bytes = 0;
+		hipError_t err = hipMallocPitch(
+			reinterpret_cast<void**>(&this->data_),
+			&pitch_bytes,
+			width * sizeof(T),
+			height
+		);
+
+		if (err != hipSuccess) {
+			throw std::runtime_error("hipMallocPitch failed");
+		}
+
+		this->pitch_ = pitch_bytes / sizeof(T);
+	}
+
+	device_unique_ptr2(const device_unique_ptr2 &) = delete;
+	auto operator=(const device_unique_ptr2 &) -> device_unique_ptr2 & = delete;
+
+	device_unique_ptr2(device_unique_ptr2 &&other) noexcept
+		: device_span2<T>(other.data_, other.width_, other.height_, other.pitch_)
+	{
+		other.data_ = nullptr;
+		other.width_ = 0;
+		other.height_ = 0;
+		other.pitch_ = 0;
+		other.size_ = 0;
+	}
+
+	auto operator=(device_unique_ptr2 &&other) noexcept -> device_unique_ptr2 & {
+		if (this != &other) {
+			this->free();
+			this->data_ = other.data_;
+			this->width_ = other.width_;
+			this->height_ = other.height_;
+			this->pitch_ = other.pitch_;
+			this->size_ = other.size_;
+			other.data_ = nullptr;
+			other.width_ = 0;
+			other.height_ = 0;
+			other.pitch_ = 0;
+			other.size_ = 0;
+		}
+		return *this;
+	}
+
+	~device_unique_ptr2() {
+		free();
+	}
+
+	operator device_span2<T>() const noexcept {
+		return device_span2<T>(this->data_, this->width_, this->height_, this->pitch_);
+	}
+
+	[[nodiscard]] constexpr auto width() const noexcept -> size_type { return this->width_; }
+	[[nodiscard]] constexpr auto height() const noexcept -> size_type { return this->height_; }
+	[[nodiscard]] constexpr auto pitch() const noexcept -> size_type { return this->pitch_; }
+
+private:
+	auto free() -> void {
+		if (!this->data_) {
+			return;
+		}
+		auto err = hipFree(this->data_);
+		if (err != hipSuccess) {
+			throw std::runtime_error("hipFree failed");
+		}
+		this->data_ = nullptr;
+		this->width_ = 0;
+		this->height_ = 0;
+		this->pitch_ = 0;
+		this->size_ = 0;
+	}
+
+	friend class device_span2<T>;
+};
+
+template <typename U>
+	requires std::is_trivially_copyable_v<U>
+auto copy_to_device(
+	const std::span<U> host,
+	const device_span2<U> device
+) -> void {
+	const U *host_ptr = host.data();
+	U *device_ptr = device.data_;
+
+	auto err = hipMemcpy2D(
+		device_ptr,
+		device.pitch_ * sizeof(U),
+		host_ptr,
+		device.width_ * sizeof(U),
+		device.width_ * sizeof(U),
+		device.height_,
+		hipMemcpyHostToDevice
+	);
+
+	if (err != hipSuccess) {
+		throw std::runtime_error("hipMemcpy2D (to device) failed");
+	}
+}
+
+template <typename U>
+	requires std::is_trivially_copyable_v<U>
+auto copy_to_host(
+	const std::span<U> host,
+	const device_span2<U> device
+) -> void {
+	U *host_ptr = host.data();
+	const U *device_ptr = device.data_;
+
+	auto err = hipMemcpy2D(
+		host_ptr,
+		device.width_ * sizeof(U),
+		device_ptr,
+		device.pitch_ * sizeof(U),
+		device.width_ * sizeof(U),
+		device.height_,
+		hipMemcpyDeviceToHost
+	);
+
+	if (err != hipSuccess) {
+		throw std::runtime_error("hipMemcpy2D (to host) failed");
+	}
+}
+
+template <typename U>
+	requires std::is_trivially_copyable_v<U>
+auto device_memset(device_span2<U> device, int val) -> void {
+	auto err = hipMemset2D(
+		device.data_,
+		device.pitch_ * sizeof(U),
+		val,
+		device.width_ * sizeof(U),
+		device.height_
+	);
+
+	if (err != hipSuccess) {
+		throw std::runtime_error("hipMemset2D failed");
+	}
+}
+
+}
---
+++ gpu_3d.hpp
@@ -0,0 +1,285 @@
+#pragma once
+
+#include "gpu.hpp"
+
+namespace gpu {
+
+template <typename T>
+	requires std::is_trivially_copyable_v<T>
+class device_span3 : public device_span<T> {
+public:
+	using base = device_span<T>;
+	using typename base::element_type;
+	using typename base::value_type;
+	using typename base::size_type;
+	using typename base::pointer;
+	using typename base::const_pointer;
+
+private:
+	size_type width_  = 0;
+	size_type height_ = 0;
+	size_type depth_  = 0;
+	size_type pitch_  = 0;
+	size_type slice_pitch_ = 0;
+
+public:
+	constexpr device_span3() noexcept = default;
+
+	constexpr device_span3(
+		pointer ptr,
+		size_type width,
+		size_type height,
+		size_type depth,
+		size_type pitch,
+		size_type slice_pitch
+	) noexcept
+		: base(ptr, width * height * depth)
+		, width_(width)
+		, height_(height)
+		, depth_(depth)
+		, pitch_(pitch)
+		, slice_pitch_(slice_pitch)
+	{}
+
+	[[nodiscard]] constexpr auto width() const noexcept -> size_type { return width_; }
+	[[nodiscard]] constexpr auto height() const noexcept -> size_type { return height_; }
+	[[nodiscard]] constexpr auto depth() const noexcept -> size_type { return depth_; }
+	[[nodiscard]] constexpr auto pitch() const noexcept -> size_type { return pitch_; }
+	[[nodiscard]] constexpr auto slice_pitch() const noexcept -> size_type { return slice_pitch_; }
+
+	[[nodiscard]] __device__ constexpr auto operator()(size_type x, size_type y, size_type z) -> T& {
+		return this->data_[z * slice_pitch_ + y * pitch_ + x];
+	}
+
+	[[nodiscard]] __device__ constexpr auto operator()(size_type x, size_type y, size_type z) const -> const T& {
+		return this->data_[z * slice_pitch_ + y * pitch_ + x];
+	}
+
+	[[nodiscard]] constexpr auto slice(size_type z) const noexcept -> device_span2<T> {
+		return device_span2<T>(
+			this->data_ + z * slice_pitch_,
+			width_,
+			height_,
+			pitch_
+		);
+	}
+
+	[[nodiscard]] constexpr auto row(size_type y, size_type z) const noexcept -> device_span<T> {
+		return device_span<T>(this->data_ + z * slice_pitch_ + y * pitch_, width_);
+	}
+
+	[[nodiscard]] constexpr auto subspan3(
+		size_type x_offset,
+		size_type y_offset,
+		size_type z_offset,
+		size_type width,
+		size_type height,
+		size_type depth
+	) const noexcept -> device_span3<T> {
+		return device_span3<T>(
+			this->data_ + z_offset * slice_pitch_ + y_offset * pitch_ + x_offset,
+			width,
+			height,
+			depth,
+			pitch_,
+			slice_pitch_
+		);
+	}
+};
+
+template <typename T>
+	requires std::is_trivially_copyable_v<T>
+class device_unique_ptr3 : private device_span3<T> {
+public:
+	using base = device_span3<T>;
+	using typename base::size_type;
+	using typename base::pointer;
+
+	device_unique_ptr3() noexcept = default;
+
+	explicit device_unique_ptr3(size_type width, size_type height, size_type depth)
+		: device_span3<T>(nullptr, width, height, depth, 0, 0)
+	{
+		if (width <= 0 || height <= 0 || depth <= 0) {
+			return;
+		}
+
+		hipExtent extent = make_hipExtent(width * sizeof(T), height, depth);
+		hipPitchedPtr pitched_ptr;
+
+		hipError_t err = hipMalloc3D(&pitched_ptr, extent);
+
+		if (err != hipSuccess) {
+			throw std::runtime_error("hipMalloc3D failed");
+		}
+
+		this->data_ = static_cast<pointer>(pitched_ptr.ptr);
+		this->pitch_ = pitched_ptr.pitch / sizeof(T);
+		this->slice_pitch_ = (pitched_ptr.pitch / sizeof(T)) * height;
+	}
+
+	device_unique_ptr3(const device_unique_ptr3 &) = delete;
+	auto operator=(const device_unique_ptr3 &) -> device_unique_ptr3 & = delete;
+
+	device_unique_ptr3(device_unique_ptr3 &&other) noexcept
+		: device_span3<T>(other.data_, other.width_, other.height_, other.depth_, other.pitch_, other.slice_pitch_)
+	{
+		other.data_ = nullptr;
+		other.width_ = 0;
+		other.height_ = 0;
+		other.depth_ = 0;
+		other.pitch_ = 0;
+		other.slice_pitch_ = 0;
+		other.size_ = 0;
+	}
+
+	auto operator=(device_unique_ptr3 &&other) noexcept -> device_unique_ptr3 & {
+		if (this != &other) {
+			this->free();
+			this->data_ = other.data_;
+			this->width_ = other.width_;
+			this->height_ = other.height_;
+			this->depth_ = other.depth_;
+			this->pitch_ = other.pitch_;
+			this->slice_pitch_ = other.slice_pitch_;
+			this->size_ = other.size_;
+			other.data_ = nullptr;
+			other.width_ = 0;
+			other.height_ = 0;
+			other.depth_ = 0;
+			other.pitch_ = 0;
+			other.slice_pitch_ = 0;
+			other.size_ = 0;
+		}
+		return *this;
+	}
+
+	~device_unique_ptr3() {
+		free();
+	}
+
+	operator device_span3<T>() const noexcept {
+		return device_span3<T>(this->data_, this->width_, this->height_, this->depth_, this->pitch_, this->slice_pitch_);
+	}
+
+	[[nodiscard]] constexpr auto width() const noexcept -> size_type { return this->width_; }
+	[[nodiscard]] constexpr auto height() const noexcept -> size_type { return this->height_; }
+	[[nodiscard]] constexpr auto depth() const noexcept -> size_type { return this->depth_; }
+	[[nodiscard]] constexpr auto pitch() const noexcept -> size_type { return this->pitch_; }
+	[[nodiscard]] constexpr auto slice_pitch() const noexcept -> size_type { return this->slice_pitch_; }
+
+private:
+	auto free() -> void {
+		if (!this->data_) {
+			return;
+		}
+		auto err = hipFree(this->data_);
+		if (err != hipSuccess) {
+			throw std::runtime_error("hipFree failed");
+		}
+		this->data_ = nullptr;
+		this->width_ = 0;
+		this->height_ = 0;
+		this->depth_ = 0;
+		this->pitch_ = 0;
+		this->slice_pitch_ = 0;
+		this->size_ = 0;
+	}
+
+	friend class device_span3<T>;
+};
+
+template <typename U>
+	requires std::is_trivially_copyable_v<U>
+auto copy_to_device(
+	const std::span<U> host,
+	const device_span3<U> device
+) -> void {
+	const U *host_ptr = host.data();
+
+	hipMemcpy3DParms params = {0};
+	params.srcPtr = make_hipPitchedPtr(
+		const_cast<U*>(host_ptr),
+		device.width_ * sizeof(U),
+		device.width_,
+		device.height_
+	);
+	params.dstPtr = make_hipPitchedPtr(
+		device.data_,
+		device.pitch_ * sizeof(U),
+		device.width_,
+		device.height_
+	);
+	params.extent = make_hipExtent(
+		device.width_ * sizeof(U),
+		device.height_,
+		device.depth_
+	);
+	params.kind = hipMemcpyHostToDevice;
+
+	auto err = hipMemcpy3D(&params);
+
+	if (err != hipSuccess) {
+		throw std::runtime_error("hipMemcpy3D (to device) failed");
+	}
+}
+
+template <typename U>
+	requires std::is_trivially_copyable_v<U>
+auto copy_to_host(
+	const std::span<U> host,
+	const device_span3<U> device
+) -> void {
+	U *host_ptr = host.data();
+
+	hipMemcpy3DParms params = {0};
+	params.srcPtr = make_hipPitchedPtr(
+		device.data_,
+		device.pitch_ * sizeof(U),
+		device.width_,
+		device.height_
+	);
+	params.dstPtr = make_hipPitchedPtr(
+		host_ptr,
+		device.width_ * sizeof(U),
+		device.width_,
+		device.height_
+	);
+	params.extent = make_hipExtent(
+		device.width_ * sizeof(U),
+		device.height_,
+		device.depth_
+	);
+	params.kind = hipMemcpyDeviceToHost;
+
+	auto err = hipMemcpy3D(&params);
+
+	if (err != hipSuccess) {
+		throw std::runtime_error("hipMemcpy3D (to host) failed");
+	}
+}
+
+template <typename U>
+	requires std::is_trivially_copyable_v<U>
+auto device_memset(device_span3<U> device, int val) -> void {
+	hipPitchedPtr pitched_ptr = make_hipPitchedPtr(
+		device.data_,
+		device.pitch_ * sizeof(U),
+		device.width_,
+		device.height_
+	);
+
+	hipExtent extent = make_hipExtent(
+		device.width_ * sizeof(U),
+		device.height_,
+		device.depth_
+	);
+
+	auto err = hipMemset3D(pitched_ptr, val, extent);
+
+	if (err != hipSuccess) {
+		throw std::runtime_error("hipMemset3D failed");
+	}
+}
+
+}
